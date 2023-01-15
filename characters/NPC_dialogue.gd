5dfa20906cd60b80a46b71f7a13a38d3084335f4
athena <mrantonioman69@gmail.com>
2023-01-14 18:58:19 -0600

    fixed the dialogue system

---
+++ characters/NPC_dialogue.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") and len(get_overlapping_bodies()) >0:
+		use_dialogue()
+		
+func use_dialogue():
+	var dialogue = get_parent().get_node("Area2D/CollisionShape2D/Dialogue")
+	
+	if dialogue:
+		dialogue.start()
